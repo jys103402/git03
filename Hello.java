ccaffae3edd30e7be15a0815278032d0b92369a0
java <java>
2017-02-02 17:25:14 +0900

    git03 test

---
+++ Hello.java
@@ -0,0 +1,16 @@
+
+public class Hello{
+
+	public static void main(String[] args){
+
+		for(int i = 0 ; i<10; i++){
+		
+			System.out.println("Hello Git");
+		}
+
+		System.out.println("3 + 4 = " + Calculator.add(3,4));
+		System.out.println("3 - 4 = " + Calculator.substrat(3,4));
+		System.out.println("3 * 4 = " + Calculator.multiply(3,4));
+		System.out.println("3 / 4 = " + Calculator.divide(3,4));
+	}
+}
